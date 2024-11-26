6bca6463d8e365c00aaf891380656e0f9d7cb42e
Dan Plubell <Dan.Plubell@coxautoinc.com>
2024-11-26 17:29:08 -0600

    add gitignore, start mandelbrot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use num::complex::Complex;
+
+fn calculate_mandelbrot(
+    max_iters: usize,
+    x_min: f64,
+    x_max: f64,
+    y_min: f64,
+    y_max: f64,
+    width: usize,
+    height: usize,
+) -> Vec<Vec<usize>> {
+    let mut rows: Vec<_> = Vec::with_capacity(width);
+    for img_y in 0..height {
+        let mut row: Vec<usize> = Vec::with_capacity(height);
+        for img_x in 0..width {
+            let x_percent = (img_x as f64) / (width as f64);
+            let y_percent = (img_y as f64) / (height as f64);
+            let cx = x_min + (x_max - x_min) * x_percent;
+            let cy = y_min + (y_max - y_min) * y_percent;
+            let escaped_at = mandelbrot_at_point(cx, cy, max_iters);
+            row.push(escaped_at);
+        }
+        rows.push(row)
+    }
+    rows
+}
+fn mandelbrot_at_point(x: f64, y: f64, max_iters: usize) -> usize {}
 fn main() {
     println!("Hello, world!");
 }
